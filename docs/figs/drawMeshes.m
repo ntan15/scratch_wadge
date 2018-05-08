06fd09e55cdb9ec9e6d2e0b7b46d5d2bc8f9e84e
Jesse Chan <jchan985@gmail.com>
2018-05-07 23:55:44 -0500

    updating docs with intro

--- docs/figs/drawMeshes.m
+++ docs/figs/drawMeshes.m
@@ -74,6 +74,7 @@ if 1
 end
 
 %% 3D meshes
+
 Globals3D
 N = 4;
 mesh = '/Users/jchan985/Desktop/wadges/meshes/periodicCube0.msh';
@@ -90,6 +91,52 @@ zz = z/10;
 x = x +   a*sin(pi*xx) .* sin(2*pi*yy).*sin(pi*zz);
 y = y - 2*a*sin(2*pi*xx).*sin(pi*yy) .* sin(2*pi*zz);
 z = z +   a*sin(pi*xx) .* sin(2*pi*yy).*sin(pi*zz);
+xb = []; yb = []; zb = [];
+for e = 1:K
+    for f = 1:4
+        if (EToE(e,f)==e)
+            xb = [xb x(Fmask(:,f),e)];
+            yb = [yb y(Fmask(:,f),e)];
+            zb = [zb z(Fmask(:,f),e)];
+        end
+    end
+end
+
+[rx,sx,tx,ry,sy,ty,rz,sz,tz,J] = GeometricFactors3D(x,y,z,Dr,Ds,Dt);
+
+r1D = linspace(-1,1,100)';
+e = ones(size(r1D));
+% rf = [r1D; r1D; -e];
+% sf = [-e; -r1D; r1D];
+% plot(rf,sf,'k.')
+% Vt = Vandermonde2D(N,rf,sf)/Vandermonde2D(N,r(Fmask(:,1)),s(Fmask(:,1)));
+% plot3(Vt*xb,Vt*yb,Vt*zb,'k.')
+
+rf = [r1D;r1D;-e; -e; r1D;  -e];
+sf = [-e; -r1D;r1D;-e; -e;  r1D];
+tf = [-e;  -e; -e; r1D;-r1D;-r1D];
+
+Vf = Vandermonde3D(N,rf,sf,tf)/V;
+plot3(Vf*x,Vf*y,Vf*z,'k.')
+
+%% TG mesh
+
+Globals3D
+N = 4;
+mesh = '/Users/jchan985/Desktop/wadges/meshes/periodicCubeTG0.msh';
+[Nv, VX, VY, VZ, K, EToV] = MeshReaderGmsh3D(mesh);
+
+StartUp3D
+
+% make curvilinear mesh (still unstable?)
+a = 1/2;
+x0 = 0; y0 = 0;
+xx = x;
+yy = y;
+zz = z;
+x = x +  a*sin(xx) .* sin(yy).*sin(zz);
+y = y +  a*sin(xx).*sin(yy) .* sin(zz);
+z = z +  a*sin(xx) .* sin(yy).*sin(zz);
 xb = []; yb = []; zb = [];
 for e = 1:K
     for f = 1:4
